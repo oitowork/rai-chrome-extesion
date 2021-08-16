1a7eae55ff3dc43ac8d458404dc96f7d4676fe50
codingsh <codingsh@pm.me>
2021-08-16 18:15:52 -0300

    feat(coingecko): add coingecko api

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Switch, FormControl, FormLabel } from "@chakra-ui/react";
+import { Switch, FormControl, FormLabel, Heading, Stack  } from "@chakra-ui/react";
+import { CoinGecko} from "coingecko-api"
 import './Popup.css';
+
 
 const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
+      <Stack spacing={8}>
+          $3.03
+      </Stack>
       <FormControl display="flex" alignItems="center">
         <FormLabel htmlFor="email-alerts" mb="0">
           Enable USD to RAI?
