ef5e471105cc044ce311b54f760edc4bb31b9bfd
codingsh <codingsh@pm.me>
2021-08-16 15:45:56 -0300

    feat(chakraProvider): add chakra-ui

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ChakraProvider } from "@chakra-ui/react";
 
 import Popup from './Popup';
 import './index.css';
 
-render(<Popup />, window.document.querySelector('#app-container'));
+render(
+    <ChakraProvider>
+        <Popup />
+    </ChakraProvider>
+, window.document.querySelector('#app-container'));
 
 if (module.hot) module.hot.accept();
