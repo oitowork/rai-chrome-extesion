7d9413d3bc1b716354682d25ac66397a4cba7b42
thayrone <thayronedepaula15@gmail.com>
2021-08-17 14:08:24 -0300

    show the info

--- src/pages/Content/modules/print.js
+++ src/pages/Content/modules/print.js
@@ -2,36 +2,42 @@ export const printLine = (line) => {
   console.log('===> FROM THE PRINT MODULE:', line);
   console.log('Thayrone');
 };
-function findPrice(node) {
-  node = node || document.body;
-  var text = node.textContent || node.innerText,
-    regex = /\$\s*[0-9,]+(?:\s*\.\s*\d{2})?/,
-    match = text.match(regex);
-  if (match) {
-    var children = node.children,
-      l = children.length,
-      i;
-    for (i = 0; i < l; i++) {
-      if (findPrice(children[i])) {
-        return children[i];
-      }
-    }
-    // if no children matched, then this is the narrowest container
-    return node;
-  }
-}
-var result = findPrice();
-console.log()
+// function findPrice(node) {
+//   node = node || document.body;
+//   var text = node.textContent || node.innerText,
+//     regex = /\$\s*[0-9,]+(?:\s*\.\s*\d{2})?/,
+//     match = text.match(regex);
 
-// Exemplo utilizando laço for
-for (var span of result.children) {
-  // console.log((span.innerText));
-}
+//   if (match) {
+//     var children = node.children,
+//       l = children.length,
+//       i;
+//     for (i = 0; i < l; i++) {
+//       if (findPrice(children[i])) {
+//         return children[i];
+//       }
+//     }
+//     // if no children matched, then this is the narrowest container
+//     return node;
+//   }
+// }
+// const result = findPrice();
 
-console.log(result.children);
-// Exemplo utilizando métodos do array
-Array.from(result.children)
-  .filter((span) => span.innerText === 'Your neth worth')
-  .forEach((span) => console.log(span));
+let regex = /\$\s*[0-9,]+(?:\s*\.\s*\d{2})?/g;
 
-alert('oi');
+window.onload(
+  (document.body.innerHTML = document.body.innerHTML.replace(
+    regex,
+    'RAI'
+  ))
+);
+
+/*
+(function () {
+  setInterval(() => {
+    document.body.innerHTML = document.body.innerHTML.replace(
+      regex,
+      'Qualquer valor'
+    );
+  }, 2000);
+})();*/
