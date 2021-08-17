397ad84ef115f94f3286113bf8929df425fd4c2a
Dani Gasp <danigaspariam@gmail.com>
2021-08-17 17:43:25 -0300

    popup estilizado

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -16,6 +16,7 @@ import {
   VStack,
   Center,
   Spacer,
+  SimpleGrid,
 } from '@chakra-ui/react';
 import './Popup.css';
 import reflexerLogo from '../../assets/img/reflexer.finance.png';
@@ -23,38 +24,76 @@ import reflexer from '../../assets/img/brand-white.svg';
 
 const Popup = () => {
   return (
-    <Center py={9}>
-      <Flex alignItems="center">
+    <Center backgroundColor="#1010" py={9}>
+      <Flex>
         <VStack
-          borderRadius="xl"
+          borderRadius="3xl"
           borderColor="green.50"
           overflow="hidden"
           maxW="sm"
           maxH="sm"
           alignItems="center"
+          justifyItems="center"
+          marginTop="10px"
         >
-          <Box alignItems="center">
+          <Box
+            marginLeft="auto"
+            marginRight="auto"
+            as="section"
+            py="10"
+            px={{ base: '4', md: '8' }}
+          >
             <Wrap>
-              <WrapItem alignItems="center">
-                <Stack spacing={3}>
-                  <Avatar
-                    size="lg"
-                    name="reflexer.finance"
-                    src={reflexerLogo}
-                  />{' '}
-                  <Text color="#68D391" fontSize="4xl">
+              <SimpleGrid
+                marginLeft="auto"
+                marginRight="auto"
+                columns={{ base: 1, lg: 3 }}
+                spacing={{ base: '8', lg: '0' }}
+                maxW="6xl"
+                mx="auto"
+                justifyItems="center"
+                alignItems="center"
+              >
+                <Text
+                  textAlign="center"
+                  marginLeft="auto"
+                  marginRight="auto"
+                  color="#101010"
+                  fontSize="2xl"
+                  fontWeight="bold"
+                >
+                  RAI
+                </Text>
+                <Stack align="center" spacing={4}>
+                  <img className="avatar" src={reflexerLogo} width="50%"></img>
+                  <Text
+                    marginLeft="auto"
+                    marginRight="auto"
+                    color="#68D391"
+                    fontSize="2xl"
+                    fontWeight="bold"
+                  >
                     $3.03
                   </Text>
                 </Stack>
-              </WrapItem>
+              </SimpleGrid>
             </Wrap>
             <Spacer />
-            <Stack align="center" direction="row">
-              <Switch size="lg" colorScheme="#68D391" />
+            <Stack marginLeft="auto" marginRight="auto" direction="row">
+              <Switch
+                width="50%"
+                marginTop="20px"
+                marginBottom="20px"
+                marginLeft="auto"
+                marginRight="auto"
+                className="switch"
+                size="lg"
+                colorScheme="#68D391"
+              />
             </Stack>
             <Spacer />
-            <Stack>
-              <img src={reflexer} alt="Reflexer"></img>
+            <Stack align="center">
+              <img src={reflexer} alt="Reflexer" width="70%"></img>
             </Stack>
           </Box>
         </VStack>
