3a21c2e72ff799fa0bbffcf8646badbd1d969357
codingsh <codingsh@pm.me>
2021-08-17 15:58:09 -0300

    chore(newTab): remove newtab on manifest and create replaceToRAI function

--- src/pages/Content/modules/print.js
+++ src/pages/Content/modules/print.js
@@ -1,36 +1,20 @@
 export const printLine = (line) => {
   console.log('===> FROM THE PRINT MODULE:', line);
-  console.log('Thayrone');
 };
-// function findPrice(node) {
-//   node = node || document.body;
-//   var text = node.textContent || node.innerText,
-//     regex = /\$\s*[0-9,]+(?:\s*\.\s*\d{2})?/,
-//     match = text.match(regex);
 
-//   if (match) {
-//     var children = node.children,
-//       l = children.length,
-//       i;
-//     for (i = 0; i < l; i++) {
-//       if (findPrice(children[i])) {
-//         return children[i];
-//       }
-//     }
-//     // if no children matched, then this is the narrowest container
-//     return node;
-//   }
-// }
-// const result = findPrice();
 
-let regex = /\$\s*[0-9,]+(?:\s*\.\s*\d{2})?/g;
-
-window.onload(
-  (document.body.innerHTML = document.body.innerHTML.replace(
+function replaceToRAI() {
+  let regex = /\$\s*[0-9,]+(?:\s*\.\s*\d{2})?/g;
+  let result = document.body.innerHTML = document.body.innerHTML.replace(
     regex,
     'RAI'
-  ))
-);
+  )
+  return result;
+}
+
+var result = replaceToRAI(); // undefined, the function does not return anything
+window.onload = result;
+
 
 /*
 (function () {
