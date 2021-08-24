7d78ef1ef98408d4c1f71356f0ad3954b4611498
thayrone <thayronedepaula15@gmail.com>
2021-08-24 14:26:23 -0300

    feat: new regex

--- src/pages/Content/modules/print.js
+++ src/pages/Content/modules/print.js
@@ -6,7 +6,7 @@ export const printLine = (line) => {
 };
 
 function toFloat(num) {
-  const cleanStr = String(num).replace(/[^0-9.,]/g, '');
+  const cleanStr = String(num).replace(/[^0-9.,-]/g, '');
   let dotPos = cleanStr.indexOf('.');
   let commaPos = cleanStr.indexOf(',');
 
@@ -39,7 +39,7 @@ function toFloat(num) {
   }
 
   return parseFloat(
-    `${integerPart.replace(/[^0-9]/g, '')}.${decimalPart.replace(
+    `${integerPart.replace(/[^0-9-]/g, '')}.${decimalPart.replace(
       /[^0-9]/g,
       ''
     )}`
@@ -59,10 +59,10 @@ function toFloatBetween0and1(num) {
     else sep = false;
   }
 
-  if (sep === false) return num.replace(/[^\d]/g, '');
+  if (sep === false) return num.replace(/[^\d-]/g, '');
 
   return (
-    num.substr(0, sep).replace(/[^\d]/g, '') +
+    num.substr(0, sep).replace(/[^\d-]/g, '') +
     '.' +
     num.substr(sep + 1, num.length).replace(/[^0-9]/, '')
   );
@@ -91,15 +91,17 @@ export const turnOFFToRAI = () => {
   console.log('Extesion Turn OFF');
 };
 const regex = [
-  /US\$\s*(\d{1,3}(\.\d{3})*|(\d+))(,\d{0,9})?/g,
-  /\$\s*(\d{1,3}(,\d{3})*|(\d+))(\.\d{0,9})?/g,
+  /\$\s*[0-9,]+\d+[K?M?B?T]/, //\\$\\d+((,|\\.)?)(\\d*)?(m)?
+  /US\$\s*?-?(\d{1,3}(\.\d{3})*|(\d+))(,\d{0,9})?/g,
+  /\$\s*?-?(\d{1,3}(.\d{3})*|(\d+))(\.\d{0,9})?/g,
+  /\$\s*?-?(\d{1,3}(,\d{3})*|(\d+))(\.\d{0,9})?/g,
 ]; //US$
 // /\$\s*[0-9,]+(?:\s*\.\s*\d{2})?/g;
 function isNumberBetween0and1(num, raiValue, allNumber) {
   let isZero = num.substr(0, 1);
   let vi = toFloat(allNumber);
   // console.log(vi);
-  if (isZero === '0') {
+  if (isZero === '0' || isZero === '-') {
     return currency(toFloatBetween0and1(allNumber), {
       symbol: '',
       decimal: '.',
@@ -118,24 +120,75 @@ function isNumberBetween0and1(num, raiValue, allNumber) {
       .format();
   }
 }
-const turnOn = async () => {
+
+export const turnOn = async () => {
   const response = await fetchAllPricesInUSD();
   const raiValue = response.RAI;
 
   regex.map((reg) => {
     return setInterval(() => {
-      findAndReplaceDOMText(document.body, {
-        find: reg,
-        preset: '',
-        forceContext: true,
-        replace: function (portion) {
-          // console.log(portion.text);
-          const usdNumber = portion.text.replace(/[^\d,.-]|/g, '');
-          const oito = isNumberBetween0and1(usdNumber, raiValue, usdNumber);
-          return `${oito} RAI`;
-        },
-      });
+      findAndReplaceDOMText(
+        document.querySelector('*:not(script):not(noscript):not(style)'),
+        {
+          find: reg,
+          portionMode: 'retain',
+          replace: function (portion) {
+            console.log(portion.text);
+            const usdNumber = portion.text.replace(/[^\d,.-]|/g, '');
+            // console.log(usdNumber);
+            const oito = isNumberBetween0and1(usdNumber, raiValue, usdNumber);
+            return `${oito}`;
+          },
+        }
+      );
     }, 1500);
   });
 };
 turnOn();
+const num = '$ 1,67B';
+const v = num.replace(/[^\d,.-]|/g, '');
+// console.log(toFloat(v) * 2);
+// chrome.runtime. .addListener(function (request, sender, sendResponse) {
+//   console.log(sendResponse);
+
+//   if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' });
+// });
+
+// seleciona o nó alvo
+let allDocument = document.body;
+
+// cria uma nova instância de observador
+let observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    // console.log(mutation);
+  });
+});
+
+// configuração do observador:
+let config = { childList: true, subtree: true };
+
+// passar o nó alvo, bem como as opções de observação
+observer.observe(allDocument, config);
+
+// mais tarde você pode parar de observar
+//observer.disconnect();
+
+// console.log(toFloat(num1));
+const replaceOnDocument = (
+  pattern,
+  string,
+  { target = document.body } = {}
+) => {
+  // Handle `string` — see the last section
+  [
+    target,
+    ...target.querySelectorAll('*:not(script):not(noscript):not(style)'),
+  ].forEach(({ childNodes: [...nodes] }) =>
+    nodes
+      .filter(({ nodeType }) => nodeType === document.TEXT_NODE)
+      .forEach(
+        (textNode) =>
+          (textNode.textContent = textNode.textContent.replace(pattern, string))
+      )
+  );
+};
